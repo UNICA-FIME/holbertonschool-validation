c4ebe552e139811353bb38330e35f552d5e5467f
user name <user email>
2023-01-07 02:01:40 +0000

    change

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 VERSION=0.109.0
 apt-get update && apt-get install -y make wget
-wget 
-https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz
 tar -xvf hugo_${VERSION}_Linux-64bit.tar.gz hugo
 mv hugo /usr/local/bin/
 rm -rf hugo_${VERSION}_Linux-64bit.tar.gz
 
-make hugo
+make build
