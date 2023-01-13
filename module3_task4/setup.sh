3826cad9d69fce738ee9555faa2189e7214dfd49
user name <user email>
2023-01-13 15:52:16 +0000

    first version

---
+++ module3_task4/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+mv hugo /usr/local/bin/
+rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+apt-get install zip -y
+npm install -g markdownlint-cli -y
+make build
